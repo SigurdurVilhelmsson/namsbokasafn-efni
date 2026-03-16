b23257bf2a256890b56e14cbd7a96e5aab8f929a
Sigurður E. Vilhelmsson <126701811+SigurdurVilhelmsson@users.noreply.github.com>
2026-03-16 14:48:22 +0000

    Update layout.js

--- server/public/js/layout.js
+++ server/public/js/layout.js
@@ -41,7 +41,7 @@
       </a>
       <a href="/editor" class="nav-link" data-paths="/editor,/segment-editor">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-        <span>Ritstjori</span>
+        <span>Ritstjóri</span>
       </a>
       <a href="/progress" class="nav-link" data-paths="/progress,/status">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
